5f1e3f12958c8b2c3b988e6fba0bb4f131422f96
Jose Maria <jmbmpp@gmail.com>
2015-07-15 17:00:07 +0200

    Creación de la Base de Datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,8 +1,8 @@
+/*
 // GET /quizes/question
 exports.question = function(req, res) {
    res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
-
 // GET /quizes/answer
 exports.answer = function(req, res) {
    if (req.query.respuesta === 'Roma'){
@@ -10,4 +10,25 @@ exports.answer = function(req, res) {
    } else {
       res.render('quizes/answer', {respuesta: 'Incorrecto'});
    }
+};
+*/
+
+var models = require('../models/models.js');
+
+// GET /quizes/question
+exports.question = function(req, res) {
+  models.Quiz.findAll().success(function(quiz) {
+    res.render('quizes/question', { pregunta: quiz[0].pregunta});
+  })
+};
+
+// GET /quizes/answer
+exports.answer = function(req, res) {
+  models.Quiz.findAll().success(function(quiz) {
+    if (req.query.respuesta === quiz[0].respuesta) {
+      res.render('quizes/answer', { respuesta: 'Correcto' });
+    } else {
+      res.render('quizes/answer', { respuesta: 'Incorrecto'});
+    }
+  })
 };
